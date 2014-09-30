37c53bd9f8b0dc52e143f504b071948af3e229b5
MNEACR <arturocadenaromero@gmail.com>
2014-09-29 19:56:36 -0500

    name is required

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -11,7 +11,10 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save category without name' do
+    category = categories(:engineer)
+    assert category.invalid?, "It should be invalid"
+    assert category.errors[:name].any?, "It should contain an error in the name"
+    assert_equal ["can't be blank"], category.errors[:name], "The error message should be 'can't be blank' and was #{category.errors[:name]}"
+  end
 end
