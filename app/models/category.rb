5a68c1f1e6a4fe2d484588b88de028051d5d71f0
MNEACR <arturocadenaromero@gmail.com>
2014-09-29 21:11:16 -0500

    one method is enough

--- app/models/category.rb
+++ app/models/category.rb
@@ -11,14 +11,10 @@
 class Category < ActiveRecord::Base
   validates_presence_of :name
 
-  before_save :strip_name
-  before_save :capitalize_name
+  before_save :strip_and_capitalize_name
 
-  def strip_name
-    self.name = self.name.strip
-  end
-
-  def capitalize_name
-    self.name  = self.name.capitalize
+  def strip_and_capitalize_name
+    name.strip!
+    name.capitalize!
   end
 end
